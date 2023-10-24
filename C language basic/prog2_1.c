f10a48bccaae0d2ebee1920fef898531685c735f
jeffgool <jeffgool@gmail.com>
2023-10-24 23:53:11 +0800

    C語言基本概述

---
+++ C language basic/prog2_1.c
@@ -0,0 +1,14 @@
+/*prog2_1*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num;
+    num=2;
+    printf("I have %d cats. \n",num);
+    printf("You have %d cats, too. \n",num);
+    system("pause");
+    return 0;
+}
+    
---
+++ C language basic/prog2_2.c
@@ -0,0 +1,15 @@
+/*prog2_2*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i=2;
+    if(i<5)
+    {
+        printf("i小於5\n");
+    }
+    system("pause");
+    return 0;
+}
+    
