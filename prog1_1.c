1d8897ac524a9291b1ede582dcae272d67cc318e
jeffgool <jeffgool@gmail.com>
2023-10-24 22:36:52 +0800

    認識C語言

---
+++ prog1_1.c
@@ -0,0 +1,10 @@
+/*prog1_1*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    system("pause");
+    return 0;
+}
---
+++ prog1_2.c
@@ -0,0 +1,13 @@
+/*prog1_2*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char ch;
+    printf("Input:");
+    scanf("%c",&ch);
+    printf("ch=%c,ASCII %d\n",ch,ch);
+    system("pause");
+    return 0;
+}
