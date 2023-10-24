9b57a81e02c5751d6d6043785560334abb934941
jeffgool <jeffgool@gmail.com>
2023-10-24 22:48:26 +0800

    認識C語言

---
+++ 認識C語言/prog1_2.c
@@ -0,0 +1,13 @@
+/*prog1_2*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char ch;
+    printf("Input:");
+    scanf("%c",&ch);
+    printf("ch=%c,The ASCII is %d\n",ch,ch);
+    system("pause");
+    return 0;
+}
