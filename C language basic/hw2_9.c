dfe87b4e971d551d50f8b2ebb392bfef5f5f572c
jeffgool <jeffgool@gmail.com>
2023-10-26 00:00:19 +0800

    C語言基本概述

---
+++ C language basic/hw2_9.c
@@ -0,0 +1,14 @@
+/*hw2_9*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i = 5;
+
+    printf("i=%d",i);
+
+    system("pause");
+    return 0;
+}
+    
---
+++ C language basic/prog2_3.c
@@ -0,0 +1,12 @@
+/*prog2_3*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num=2;
+    printf("I have %d cats.\n",num);
+    system("pause");
+    return 0;
+}
+    
